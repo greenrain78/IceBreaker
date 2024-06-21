ad5045c9f4d1dbe1edb23c506993702e1948288d
GreenRain <47217640+greenrain78@users.noreply.github.com>
2024-06-21 19:44:18 +0900

    fix: .env 삭제

--- src/main/java/org/example/IceBreaking/config/AppEnvConfig.java
+++ src/main/java/org/example/IceBreaking/config/AppEnvConfig.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Component;
 @Getter
 @Component
 public class AppEnvConfig {
-    @Value("${API_KEY}")
+    @Value("${API_KEY:None}")
     private String apiKey;
 }
