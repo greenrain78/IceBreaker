549a8a92dce86c80d5a9c87d029ec2d76d7c85d1
GreenRain <47217640+greenrain78@users.noreply.github.com>
2024-06-27 00:24:24 +0900

    기본 구성 완료

--- src/main/java/com/example/IceBreaking/controller/HomeController.java
+++ src/main/java/com/example/IceBreaking/controller/HomeController.java
@@ -5,9 +5,15 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Slf4j
 @Controller
@@ -17,6 +23,17 @@ public class HomeController {
     @GetMapping("/")
     @Operation(summary = "home controller")
     public String home(Model model) {
+
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        model.addAttribute("role", role);
+        model.addAttribute("name", authentication.getName());
         log.info("home controller" + model);
         return "home";
     }
