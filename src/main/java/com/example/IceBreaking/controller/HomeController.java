2b6048a5dcd47386b8dab88f1167c30d8fabc15f
GreenRain <47217640+greenrain78@users.noreply.github.com>
2024-06-26 19:56:50 +0900

    home controller 구성

---
+++ src/main/java/com/example/IceBreaking/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.IceBreaking.controller;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@Tag(name = "home", description = "home controller")
+public class HomeController {
+    @GetMapping("/")
+    @Operation(summary = "home controller")
+    public String home(Model model) {
+        log.info("home controller" + model);
+        return "home";
+    }
+}
