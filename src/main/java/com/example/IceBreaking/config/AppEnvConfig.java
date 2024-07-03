4dbd7e9f0b00f2f0412facf0869fcdf37f3b2a72
GreenRain <47217640+greenrain78@users.noreply.github.com>
2024-07-03 22:54:37 +0900

    키 설정 재시도 - 롤백

--- src/main/java/com/example/IceBreaking/config/AppEnvConfig.java
+++ src/main/java/com/example/IceBreaking/config/AppEnvConfig.java
@@ -22,43 +22,4 @@ public class AppEnvConfig {
 
     @Value("${SONNET_API_KEY:None}")
     private String sonnetApiKey;
-
-    @Value("${ADMIN_EMAIL:None}")
-    private String adminEmail;
-
-    public void refreshSonnetAPI() {
-        log.info("Refreshing Sonnet API Key... - adminEmail: " + adminEmail);
-        try {
-            // gcloud auth print-access-token 명령 실행
-            ProcessBuilder processBuilder = new ProcessBuilder();
-            processBuilder.command("gcloud", "auth", "list");
-            Process process = processBuilder.start();
-
-            // 명령 출력 읽기
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            StringBuilder output = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                output.append(line);
-            }
-
-            // 프로세스 종료 코드 확인
-            int exitCode = process.waitFor();
-            if (exitCode == 0) {
-                // 성공적으로 액세스 토큰을 가져온 경우
-                sonnetApiKey = output.toString();
-                log.info("Sonnet API Key: " + sonnetApiKey);
-            } else {
-                // 에러가 발생한 경우
-                log.error("Error: Failed to get access token - default: " + sonnetApiKey);
-                log.error("Error: " + output);
-            }
-        } catch (IOException | InterruptedException e) {
-            log.error("Failed to get access token - default: " + sonnetApiKey, e);
-        }
-    }
-    @PostConstruct
-    public void init() {
-        refreshSonnetAPI();
-    }
 }
